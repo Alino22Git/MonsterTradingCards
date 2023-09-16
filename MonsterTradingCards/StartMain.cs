7288f42a8e717ed37fe92557371a94babe63e191
Alino22Git <hajra.alen@hotmail.com>
2023-09-16 15:22:20 +0200

    Outsource main and implementing empty class GameLogic

---
+++ MonsterTradingCards/StartMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonsterTradingCards.Classes;
+using MonsterTradingCards.GameLogic;
+
+
+namespace MonsterTradingCards
+{
+    internal class StartMain
+    {
+        static void Main()
+        {
+            GameLogic.startGame();
+        }
+    }
+
+}
