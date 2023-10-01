e59dd0fc6f9b5a7bbe067714a109b1ef86724bb0
Alino22Git <hajra.alen@hotmail.com>
2023-10-01 19:03:26 +0200

    Created Function-Templates for every Path

--- MonsterTradingCards/REST-Interface/Server.cs
+++ MonsterTradingCards/REST-Interface/Server.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Reflection.PortableExecutable;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,21 +15,22 @@ namespace MonsterTradingCards.REST_Interface
     {
         public void RunServer()
         {
-            // Definieren Sie die URL und den Port für Ihren Server
+            //Define a port/url for the server
             string url = "http://localhost:10001/";
 
-            // Erstellen und starten Sie den HTTP-Listener
+            //Create HttpListener
             HttpListener listener = new HttpListener();
             listener.Prefixes.Add(url);
             listener.Start();
 
-            Console.WriteLine("Server läuft. Warten auf Anfragen...");
+            Console.WriteLine("Server running. Waiting for Requests...");
 
+            //Server running in a loop
             while (true)
             {
                 try
                 {
-                    // Warten Sie auf eine eingehende Anforderung
+                    //Wait for request
                     HttpListenerContext context = listener.GetContext();
 
                     // Verarbeiten Sie die Anforderung in einem separaten Thread
@@ -37,45 +39,28 @@ namespace MonsterTradingCards.REST_Interface
                         HttpListenerRequest request = context.Request;
                         HttpListenerResponse response = context.Response;
 
-                        string responseString = "";
 
+                        string responseString = "";
                         if (request.HttpMethod == "GET")
                         {
-                            if (request.Url.AbsolutePath == "/users")
-                            {
-                                // Behandeln Sie GET-Anfragen für den /users-Endpunkt hier
-                                responseString = "GET-Anfrage für /users empfangen";
-                            }
-                            else if (request.Url.AbsolutePath == "/posts")
-                            {
-                                // Behandeln Sie GET-Anfragen für den /posts-Endpunkt hier
-                                responseString = "GET-Anfrage für /posts empfangen";
-                            }
+                            responseString = GetMethods(request);
                         }
                         else if (request.HttpMethod == "POST")
                         {
-                            if (request.Url.AbsolutePath == "/createUser")
-                            {
-                                // Behandeln Sie POST-Anfragen für den /createUser-Endpunkt hier
-                                using (StreamReader reader = new StreamReader(request.InputStream))
-                                {
-                                    string requestBody = reader.ReadToEnd();
-                                    User user = JsonConvert.DeserializeObject<User>(requestBody);
-                                    // Verarbeiten Sie das empfangene Benutzerobjekt
-                                    responseString =
-                                        $"POST-Anfrage für /createUser empfangen: Username = {user.Username}, Password = {user.PasswordHash}";
-                                }
-                            }
-                            else if (request.Url.AbsolutePath == "/createPost")
-                            {
-                                // Behandeln Sie POST-Anfragen für den /createPost-Endpunkt hier
-                                using (StreamReader reader = new StreamReader(request.InputStream))
-                                {
-                                    string requestBody = reader.ReadToEnd();
-                                    // Verarbeiten Sie das empfangene Post-Objekt
-                                    responseString = "POST-Anfrage für /createPost empfangen";
-                                }
-                            }
+                            responseString = PostMethods(request);
+                        }
+                        else if (request.HttpMethod == "PUT")
+                        {
+                            responseString = PutMethods(request);
+                        }
+                        else if (request.HttpMethod == "DELETE")
+                        {
+                            responseString = DeleteMethods(request);
+                        }
+
+                        if (responseString == "User not found")
+                        {
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
                         }
 
                         byte[] buffer = Encoding.UTF8.GetBytes(responseString);
@@ -87,9 +72,151 @@ namespace MonsterTradingCards.REST_Interface
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("Fehler: " + ex.Message);
+                    Console.WriteLine("Error: " + ex.Message);
                 }
             }
+
+        }
+
+
+
+
+        public string PostMethods(HttpListenerRequest request)
+        {
+            string response = "";
+            if (request.Url.AbsolutePath == "/TEST")
+            {
+                using (StreamReader reader = new StreamReader(request.InputStream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    User user = JsonConvert.DeserializeObject<User>(requestBody);
+
+                    response =$"POST-Anfrage für /createUser empfangen: Username = {user.Username}, Password = {user.PasswordHash}";
+                }
+            }
+            else if (request.Url.AbsolutePath == "/users")
+            {
+                response = "Post /users";
+            }
+            else if (request.Url.AbsolutePath == "/sessions")
+            {
+                response = "Post /sessions";
+            }
+            else if (request.Url.AbsolutePath == "/transactions/packages")
+            {
+                response = "Post /transactions/packages";
+            }
+            else if (request.Url.AbsolutePath == "/tradings")
+            {
+                response = "Post /tradings";
+            }
+            else if (request.Url.AbsolutePath == "/packages")
+            {
+                response = "Post /packages";
+            }
+            else if (request.Url.AbsolutePath == "/battles")
+            {
+                response = "Post /battles";
+            }
+            else if (request.Url.AbsolutePath == "/tradings")
+            {
+                response = "Post /tradings";
+            }
+            else if (request.Url.AbsolutePath.StartsWith("/tradings/"))
+            {
+                response = "Post /tradings/{tradingdealid}";
+            }
+
+            return response;
+        }
+
+        public string GetMethods(HttpListenerRequest request)
+        {
+            string response = "";
+            if (request.Url.AbsolutePath == "/users")
+            {
+                // Behandeln Sie GET-Anfragen für den /users-Endpunkt hier
+                response = "GET-Anfrage für /users empfangen";
+            }
+            else if (request.Url.AbsolutePath.StartsWith("/users/"))
+            {
+                
+                    //Get /{username}
+                    string username = request.Url.AbsolutePath.Substring("/users/".Length);
+
+                    // Hier sollten Sie die Benutzerdaten aus Ihrer Datenquelle abrufen!!!!!!!!!!!!!!!!!
+                    // und das Ergebnis in UserData konvertieren.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                    //!!!!UserData userData = GetUserData(username);
+                    string userData = username;
+
+                    if (userData != null)
+                    {
+                        //Send created user
+                        string jsonResponse = JsonConvert.SerializeObject(userData);
+                        response = jsonResponse;
+                    }
+                    else
+                    {
+                        response = "User not found.";
+                    }
+                }
+                else if (request.Url.AbsolutePath == "/deck")
+                {
+                    response = "Get /deck";
+            }
+                else if (request.Url.AbsolutePath == "/cards")
+                {
+                    response = "Get /cards";
+            }
+                else if (request.Url.AbsolutePath == "/stats")
+                {
+                    response = "Get /stats";
+            }
+                else if (request.Url.AbsolutePath == "/scoreboard")
+                {
+                    response = "Get /scoreboard";
+            }
+                else if (request.Url.AbsolutePath == "/tradings")
+                {
+                    response = "Get /tradings";
+            }
+
+            return response;
+        }
+
+
+
+        public string PutMethods(HttpListenerRequest request)
+            {
+            string response = "";
+            if (request.Url.AbsolutePath == "/users")
+            {
+                response = "Put /users";
+            }
+            else if (request.Url.AbsolutePath.StartsWith("/users/"))
+            {
+                response = "Put /users/{username}";
+            }
+            else if(request.Url.AbsolutePath == "/deck")
+            {
+                response = "Put /deck";
+            }
+
+            return response;
+            }
+
+        public string DeleteMethods(HttpListenerRequest request)
+        {
+            string response = "";
+
+            if (request.Url.AbsolutePath.StartsWith("/tradings/"))
+            {
+                response = "Delete /tradings/{tradingdealid}";
+            }
+            return response;
         }
     }
+
 }
+
