94faa1e00da8d3bbd54fa24ea796d7273535d445
Alino22Git <hajra.alen@hotmail.com>
2023-12-29 22:10:07 +0100

    Started Coding Trading Class

---
+++ MonsterTradingCards/BasicClasses/Trade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCards.BasicClasses
+{
+    public class Trade
+    {
+        private string Id { get; set; }
+        private string CardToTrade { get; set; }
+        private string Type { get; set; }
+        private int MinimumDamage { get; set; }
+
+        public Trade(string id, string cardToTrade, string type, int minimumDamage)
+        {
+            Id = id;
+            CardToTrade = cardToTrade;
+            Type = type;
+            MinimumDamage = minimumDamage;
+        }
+    }
+}
